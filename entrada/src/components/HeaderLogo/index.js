90aad03773e094491f87c43720c28873df8fe24d
AnnaMhairi <anna.m.macdonald@gmail.com>
2017-04-01 20:03:44 -0700

    Make logo redirect to index page

--- entrada/src/components/HeaderLogo/index.js
+++ entrada/src/components/HeaderLogo/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
+import Index from '../Index'
 import './index.css'
 
 export default class HeaderLogo extends React.Component {
+  renderIndex () {
+    return (
+      ReactDOM.render(<Index />, document.getElementById('index'))
+    )
+  }
+
   render () {
     return (
       <div className='header__logo'>
-        <a className='header__logo__left' href={'/'}>Entrada</a>
-        <a className='header__logo__right' href={'/'}>Travel</a>
+        <a className='header__logo__left' onClick={ this.renderIndex }>Entrada</a>
+        <a className='header__logo__right' onClick={ this.renderIndex }>Travel</a>
       </div>
     )
   }
