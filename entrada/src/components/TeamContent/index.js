2ad4021eb6bbb229b3be4647ad298dcaad1cf119
AnnaMhairi <anna.m.macdonald@gmail.com>
2017-04-09 11:54:35 -0700

    Update styling of team page

--- entrada/src/components/TeamContent/index.js
+++ entrada/src/components/TeamContent/index.js
@@ -9,23 +9,32 @@ export default class TeamContent extends React.Component {
         	Management team
         </div>
         <div className="about__copy">
-          We are an elite team founded in Venice Beach, distributed globally, commited to serving those on their journey to enrich the world.
+          Our small team of elite agents is distributed globally to focus on delivering the service you expect around the clock.
+          <p>Originally built as an internal solution to our own unique and complex travel needs, the Entrada Travel team realized we could offer a service that uniquely serves the small number of elite travel groups like ourselves.</p>
         </div>
         <div className="about__team">
           <div className="about__team__member">
           <div className="about__team__member__info">
             <div className="about__team__member__info__name">Anthony Gonzales</div>
             <div className="about__team__member__info__role">CEO</div>
-            <div className="about__team__member__info__bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</div>
+            <div className="about__team__member__info__bio">
+              Prior to founding Entrada, Anthony spent his time managing travel for artist and entertainer Drake, who he still works with today in a multitude of capacities. This journey has taken him across the globe several times, organizing complicated logistics for large groups. With a strong first-hand experience, he built Entrada Travel as superior service for the most discerning clients. Anthony is an active meditator and yoga practitioner who enjoys all things creative.
+            </div>
           </div>
           </div>
           <div className="about__team__member">
             <div className="about__team__member__info">
               <div className="about__team__member__info__name">Tony Sheng</div>
               <div className="about__team__member__info__role">COO</div>
-              <div className="about__team__member__info__bio">With a degree in Neuroscience from Amherst College, Tony has always been interested in how things work and how to make them better. He started his career at McKinsey & Company, helping dozens of Fortune 500 companies debug and resolve their biggest operational challenges. Since then, Tony has been building and growing tech products on the forefront of social, media, and entertainment.</div>
+              <div className="about__team__member__info__bio">
+                With a degree in Neuroscience from Amherst College, Tony has always been interested in how things work and how to make them better. He started his career at McKinsey & Company, helping dozens of Fortune 500 companies debug and resolve their biggest operational challenges. Since then, Tony has been building and growing tech products on the forefront of social, media, and entertainment.
+              </div>
             </div>
           </div>
+          </div>
+          <div className='giving'>
+            <div className='giving__header'>Giving back</div>
+            <div className='giving__copy'>Entrada Travel is passionate about seeing the world and understands the transformative experience travel offers. Understanding the hefty costs associated with travel, we work with BLANK to help subsidize class trips around the globe for underprivileged urban youth.</div>
           </div>
         	<div className='about__cta'>
         		<a className='button' href='./contact.html'>Contact us</a>
